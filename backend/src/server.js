2bb0849b5d4cb404fc2f1268e50abdc001523bdb
Ravindra Jadhav <jadhaor181@gmail.com>
2025-11-26 02:15:53 +0530

    Backend Deployment Updates

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,15 @@
 require("dotenv").config();
 const app = require("./app");
 
-const PORT = process.env.DB_PORT || 5000;
+const PORT = process.env.PORT || 5000;
+
+const cors = require("cors");
+app.use(cors({
+  origin: "*",        // or frontend domain
+  methods: "GET,POST,PUT,DELETE",
+  credentials: false
+}));
+
 
 
 app.listen(PORT, () => {
